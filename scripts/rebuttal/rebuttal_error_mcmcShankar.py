d41b0a841ac6c4033e69f7c869321f06687800ad
ShixingWang <wangshixing@wustl.edu>
2024-04-02 01:23:42 -0500

    functions to perform mcmc

--- scripts/rebuttal/rebuttal_error_mcmcShankar.py
+++ scripts/rebuttal/rebuttal_error_mcmcShankar.py
@@ -5,7 +5,7 @@
 import numpy as np
 import pandas as pd
 from pathlib import Path
-from skimage import io,util
+from skimage import io,util,morphology
 import h5py
 
 organelles = [
@@ -19,7 +19,7 @@ organelles = [
 ] 
 
 # %%
-path = r"D:\Documents\GitHub\ShixingWang\organelle-measure\images\preprocessed\EYrainbow_glucose_largerBF\probability_mitochondria_EYrainbow_glu-100_field-3.h5"
+path = "/images/preprocessed/EYrainbow_glucose_largerBF/probability_mitochondria_EYrainbow_glu-100_field-3.h5"
 
 with h5py.File(str(path)) as h5:
 	image = h5["exported_data"][1]
@@ -32,11 +32,39 @@ io.imsave(
 )
 
 # %%
-mask_selected = (image>0.5)
+def upsample(mask,prob):
+	dilated = morphology.binary_dilation(mask)
+	edge = np.logical_xor(mask,dilated)
+	to_compare = prob[edge]
+	randoms  = np.random.random(to_compare.shape)
+	compared = (to_compare > randoms)
+	mask[edge] = compared
+	return None
 
+def downsample(mask,prob):
+	eroded = morphology.binary_erosion(mask)
+	edge = np.logical_xor(mask,eroded)
+	to_compare = 1 - prob[edge]
+	randoms  = np.random.random(to_compare.shape)
+	compared = (to_compare > randoms)
+	mask[edge] = compared
+	return None
+
+# %%
+mask_selected = (image>0.5)
 N_sample = 10000
-sizes = np.empty(N_sample)
+
+mask_up = np.copy(mask_selected)
+sizes_up = np.empty(N_sample)
 for i in range(N_sample):
+	upsample(mask_up,image)
+	sizes_up[i] = np.count_nonzero(mask_up)
+	continue
+
+mask_dw = np.copy(mask_selected)
+sizes_dw = np.empty(N_sample)
+for i in range(N_sample):
+	continue
 	
 
 
